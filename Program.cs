3a6bd2de78b939571c31f5d5574c78c2b38000da
qdedm <qdedmangg@gmail.com>
2024-12-26 20:08:06 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a} + {b} = {a + b}");
+
+            //2
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a} + {b} = {b} + {a}");
+
+            //3
+            //Console.Write("a=");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a:f1} * {b:f1} = {a * b:f1}");
+
+            //4
+            //Console.Write("a=");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("c=");
+            //double c = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"({a:f2} + {b:f2}) = {a:f2} + ({b:f2} + {c:f2})");
+
+            //5
+            //Console.Write("a=");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("c=");
+            //double c = double.Parse(Console.ReadLine());
+            //Console.Write("d=");
+            //double d = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a:f2}/{b:f2}/{c:f2}/{d:f2}={a/b/c/d:f2}");
+
+            //6
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"a+b={a}+{b}={a+b}     a-b={a}-{b}={a-b}     a*b={a}*{b}={a*b}");
+
+            //7 
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write("c=");
+            //int c = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"[a*(b+c)]=[(b+c)*a]: [{a}*({b}+{c})]=[({b}+{c})*{a}] = {a*b+c}");
+
+            //8
+            //Console.Write("a=");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("c=");
+            //double c = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"({a}+({b}+{c}))=({a}+{c}+{b})");
+            Console.ReadKey();
+
+        }
+    }
+}
